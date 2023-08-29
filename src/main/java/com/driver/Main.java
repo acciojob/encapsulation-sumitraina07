1969ba79c020f167edb400f4b6c501f35b7cf061
sumeetraina <sumitraina07@gmail.com>
2023-08-29 19:18:28 +0530

    First Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,17 @@
 package com.driver;
 
 public class Main {
-  
+
+    public static void main(String[] args){
+
+        RWOnly R = new RWOnly();
+
+        //R.name = "sumit";
+        //System.out.println(R.name);
+        //since, name is private, we cannot set its value directly here
+
+        R.setName("sumit");
+
+        System.out.println(R.getName());
+    }
 }
